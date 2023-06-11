65b40139b19de58b2175cddd4e725ca6a9a5e240
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-06-11 19:09:06 -0300

    feat:adding header

---
+++ src/app/shared/modules/material/material.module.ts
@@ -0,0 +1,77 @@
+import { NgModule } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatSortModule} from "@angular/material/sort";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatStepperModule} from "@angular/material/stepper";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatChipsModule} from "@angular/material/chips";
+
+
+@NgModule({
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    DragDropModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatExpansionModule
+  ],
+  exports:[
+    DragDropModule,
+    MatInputModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatChipsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatStepperModule,
+  ]
+})
+export class MaterialModule { }
